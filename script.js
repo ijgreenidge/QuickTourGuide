91e36406e40831ee9f55de2d2063f840691d956c
ijgreenidge <igreenidge2018@bklawtech.com>
2017-04-04 21:48:27 +0000

    week 2

--- script.js
+++ script.js
@@ -1,53 +1,106 @@
- var searchterm;
+var searchBox;
+
+function initAutocomplete() {
+ var map = new google.maps.Map(document.getElementById('map'), {
+  center: {
+   lat: -33.8688,
+   lng: 151.2195
+  },
+  zoom: 13,
+  mapTypeId: 'roadmap'
+ });
+
+ // Create the search box and link it to the UI element.
+ var input = document.getElementById('city-input');
+ searchBox = new google.maps.places.SearchBox(input);
+ console.log(searchBox);
+
+ // Bias the SearchBox results towards current map's viewport.
+ map.addListener('bounds_changed', function() {
+  searchBox.setBounds(map.getBounds());
+ });
+
+ var markers = [];
+ // Listen for the event fired when the user selects a prediction and retrieve
+ // more details for that place.
+ searchBox.addListener('places_changed', function() {
+  var places = searchBox.getPlaces();
+
+  if (places.length == 0) {
+   return;
+  }
+
+  // Clear out the old markers.
+  markers.forEach(function(marker) {
+   marker.setMap(null);
+  });
+  markers = [];
+
+  // For each place, get the icon, name and location.
+  var bounds = new google.maps.LatLngBounds();
+  places.forEach(function(place) {
+   if (!place.geometry) {
+    console.log("Returned place contains no geometry");
+    return;
+   }
+   var icon = {
+    url: place.icon,
+    size: new google.maps.Size(71, 71),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(17, 34),
+    scaledSize: new google.maps.Size(25, 25)
+   };
+
+   // Create a marker for each place.
+   markers.push(new google.maps.Marker({
+    map: map,
+    icon: icon,
+    title: place.name,
+    position: place.geometry.location
+   }));
+
+   if (place.geometry.viewport) {
+    // Only geocodes have viewport.
+    bounds.union(place.geometry.viewport);
+   }
+   else {
+    bounds.extend(place.geometry.location);
+   }
+  });
+  map.fitBounds(bounds);
+ });
+ $("#go").click(function() {
+  //searchBox.trigger('places_changed');
+
+  var searchTerm = $("#city-input").val();
+  console.log(searchTerm);
+   $.getJSON("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=d0276e79c493dbc248d527abad92ae33&text=" + searchTerm + "&format=json",
+   
+   
+     function(response) { 
+      for (var i = 0; i < photos.photo.length; i++) {
+       var id = photos.photo[i].id
+       var serverId = photos.photo[i].server;
+       var farmId = photos.photo[i].farm;
+       var secret = photos.photo[i].secret;
+       "https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg"
+
+       $("#photos").append("<img src="+ "https://farm"+farmId+".staticflickr.com/"+serverId+"/"+id+"_"+secret+".jpg" +"/>")
+      }
+    });
+
+ });
+ $("#city-input").keypress(function(e) {
+  if (e.keyCode == 13)
+   $("#go").click();
+ });
+}
 
 
- var map;
- var infowindow;
-
- function initMap() {
-     var pyrmont = {
-         lat: -33.867,
-         lng: 151.195
-     };
-
-     map = new google.maps.Map(document.getElementById('map'), {
-         center: pyrmont,
-         zoom: 15
-     });
-
-     infowindow = new google.maps.InfoWindow();
-     var service = new google.maps.places.PlacesService(map);
-     service.nearbySearch({
-         location: pyrmont,
-         radius: 500,
-         type: ['sublocality']
-     }, callback);
- }
-
- function callback(results, status) {
-     if (status === google.maps.places.PlacesServiceStatus.OK) {
-         for (var i = 0; i < results.length; i++) {
-             createMarker(results[i]);
-         }
-     }
- }
-
- function createMarker(place) {
-     var placeLoc = place.geometry.location;
-     var marker = new google.maps.Marker({
-         map: map,
-         position: place.geometry.location
-     });
-
-     google.maps.event.addListener(marker, 'click', function() {
-         infowindow.setContent(place.name);
-         infowindow.open(map, this);
-     });
- }
 
 
-     // Flickr API KEY --> d0276e79c493dbc248d527abad92ae33
-     // Google Map API KEY --> AIzaSyDLeay - 3 D4F8f2RnSTncDkF5tOxm0naI_4
 
-     // "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=d0276e79c493dbc248d527abad92ae33"
-     
+// Flickr API KEY --> d0276e79c493dbc248d527abad92ae33
+// Google Map API KEY --> AIzaSyDLeay - 3 D4F8f2RnSTncDkF5tOxm0naI_4
+
+// "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=d0276e79c493dbc248d527abad92ae33&text=miami&format=json"
