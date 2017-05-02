81864ddc42f41af2119059e94bc6370a85fda5d0
ijgreenidge <igreenidge2018@bklawtech.com>
2017-05-02 21:54:52 +0000

    update 7

--- script.js
+++ script.js
@@ -1,27 +1,68 @@
 /*global google $*/
 
-function start() {
-    // 2. Initialize the JavaScript client library.
-    gapi.client.init({
-        'apiKey': 'AIzaSyAj-nd5Dm7AVo2M1zOSL2Qp00oGJDOefs0',
-        // Your API key will be automatically added to the Discovery Document URLs.
-        'discoveryDocs': ['https://people.googleapis.com/$discovery/rest'],
-        // clientId and scope are optional if auth is not required.
-        'clientId': 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
-        'scope': 'profile',
-    }).then(function() {
-        // 3. Initialize and make the API request.
-        return gapi.client.people.people.get({
-            resourceName: 'people/me'
-        });
-    }).then(function(response) {
-        console.log(response.result);
-    }, function(reason) {
-        console.log('Error: ' + reason.result.error.message);
-    });
-};
+jQuery(document).ready(function ($) {
 
-gapi.load('client', start);
+  //$('#searchquery').keyup(function () {
+
+    // the search term
+    var q = "USA";
+
+    // YouTube Data API base URL (JSON response)
+    var url = "http://gdata.youtube.com/feeds/api/videos/?v=2&alt=jsonc&callback=?"
+
+    // set paid-content as false to hide movie rentals
+    url = url + '&paid-content=false';
+
+    // set duration as long to filter partial uploads
+    url = url + '&duration=long';
+
+    // order search results by view count
+    url = url + '&orderby=viewCount';
+
+    // we can request a maximum of 50 search results in a batch
+    url = url + '&max-results=50';
+
+    $.getJSON(url + "&q=" + q, function (json) {
+
+      var count = 0;
+
+      if (json.data.items) {
+
+        var items = json.data.items;
+        var html = "";
+
+        items.forEach(function (item) {
+
+          // Check the duration of the video, 
+          // full-length movies are generally longer than 1 hour
+          var duration = Math.round((item.duration) / (60 * 60));
+
+          // Filter out videos that aren't in the Film or Movies category
+          if ((duration > 1) && (item.category == "Movies" || item.category == "Film")) {
+
+            // Include the YouTube Watch URL youtu.be 
+            html += '<p><a href="http://youtu.be/' + item.id + '">';
+
+            // Add the default video thumbnail (default quality)
+            html += '<img src="http://i.ytimg.com/vi/' + item.id + '/default.jpg">';
+
+            // Add the video title and the duration
+            html += '<h2>' + item.title + ' ' + item.duration + '</h2></a></p>';
+            count++;
+          }
+        });
+      }
+
+      // Did YouTube return any search results?
+      if (count === 0) {
+        console.log("No videos found");
+      } else {
+        // Display the YouTube search results
+        console.log(html);
+      }
+    });
+  //});
+});
 
 var searchBox;
 
